91244dabdf8778b9e589daf2ea8f211e71226ea1
Mykhailo Aliinyk <m.aliynik@gmail.com>
2022-10-13 13:30:51 -0400

    Initial structure

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { CssBaseline, Container, ThemeProvider, createTheme } from '@mui/material';
+
+import Main from '~/Main';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+const App = () => {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container maxWidth={false}>
+        <Main />
+      </Container>
+    </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '~': path.resolve('./src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.LOCAL_ENV': JSON.stringify(process.env.LOCAL_ENV),
+      'process.env.NETLIFY_SERVER': JSON.stringify(process.env.NETLIFY_SERVER),
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.ejs',
+      templateParameters: {
+        LOCAL_ENV: process.env.LOCAL_ENV || false
+      },
+    })
+  ],
+  devServer: {
+    hot: true,
+    port: 9212,
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, 'public')
+    },
+  }
+};
